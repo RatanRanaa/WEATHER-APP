ac11373ae3fec5926d477a3d3ce18aa4f84732d0
Ratan Rana <ratanrana206@gmail.com>
2023-03-04 10:53:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Result from './Components/Result';
+import Search from './Components/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='max-w-[1240px] mx-auto mt-2 p-3 shadow-xl'>
+        <Search />
+        <Result />
+      </div>
+    </>
   );
 }
 
