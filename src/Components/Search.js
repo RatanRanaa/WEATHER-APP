22efb4ef730660e7ed0bee9745f38333e4dce814
Ratan Rana <ratanrana206@gmail.com>
2023-03-06 09:52:03 +0530

    API'S data

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
-const Search = () => {
+const Search = (props) => {
+  const searchInput = useRef()
   return (
-    <div>Search</div>
+    <div className=' flex shadow-xl'>
+      <input type="search" value={props.searchData} className=' border border-black w-full p-3 text-2xl' 
+      onChange={()=>props.eventHandler(searchInput.current.value)} ref={searchInput}/>
+      <button onClick={props.searchWeather} className='p-3 bg-slate-600 text-white'>Search</button>
+    </div>
   )
 }
 
